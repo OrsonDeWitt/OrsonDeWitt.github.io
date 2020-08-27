f35f4c1a36724eaf8e37800a905697c26e95f406
OrsonDeWitt <26813733+OrsonDeWitt@users.noreply.github.com>
2020-08-27 18:34:17 +0200

    Update faq.js

--- assets/faq.js
+++ assets/faq.js
@@ -1,8 +1,18 @@
-const items = document.querySelectorAll(".accordion a");
- 
-function toggleAccordion(){
-  this.classList.toggle('active');
-  this.nextElementSibling.classList.toggle('active');
-}
- 
-items.forEach(item => item.addEventListener('click', toggleAccordion));
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+} 
